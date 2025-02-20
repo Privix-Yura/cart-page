350f09e50590837bb5fcb2a913f9f8264c3cfb5f
Privix-Yura <yusrakhurram2007@gmail.com>
2025-02-20 10:15:58 +0500

    Delete code directory

--- code/loginpage/script.js
+++
@@ -1,5 +0,0 @@
-let submit = document.getElementById("submit")
-let login = document.getElementById("login")
-let user_info = JSON.parse(localStorage.getItem("user information")) || []
-let user_information = ()
-localStorage.setItem("user information", JSON.stringify(basket))
