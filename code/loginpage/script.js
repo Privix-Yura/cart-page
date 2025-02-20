da6cde4d68b218bf9738faf26b9ad5269a20f223
Privix-Yura <yusrakhurram2007@gmail.com>
2025-02-20 09:59:47 +0500

    added files

---
+++ code/loginpage/script.js
@@ -0,0 +1,5 @@
+let submit = document.getElementById("submit")
+let login = document.getElementById("login")
+let user_info = JSON.parse(localStorage.getItem("user information")) || []
+let user_information = ()
+localStorage.setItem("user information", JSON.stringify(basket))
